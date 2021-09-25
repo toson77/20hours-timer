c161a5c905023a4b122b98247880f7392dcddad8
Noriyuki <norifxsc@gmail.com>
2021-09-25 13:31:33 +0900

    init

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+  <v-row
+    justify="center"
+    align="center"
+  >
+    <v-col
+      cols="12"
+      sm="8"
+      md="6"
+    >
       <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
         <VuetifyLogo />
