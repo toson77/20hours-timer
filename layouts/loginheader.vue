185bd28de4bef0d189993a0699f830dcd2d557c1
shunya1810 <shun1810@gmail.com>
2021-10-09 10:54:28 +0900

    更新

---
+++ layouts/loginheader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-app-bar
+      elevation="1"
+      height="40px"
+    >
+      <v-toolbar-title>
+        {{ appName }}
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-toolbar-items
+        v-for="(link, i) in links"
+        :key="i"
+      >
+        <v-btn
+          :to="link.to"
+          text
+          class="font-weight-bold"
+        >
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    appName: "20時間タイマー",
+    links: [
+      { title: '新規会員', to: '/newuser' },
+      { title: 'ログイン', to: '/login' }
+    ]
+  })
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,67 @@
 <template>
+  <v-app>
+    <my-header />
+      <v-container fluid>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            class="my-8 text-center"
+          >
+            <h1 class="text-h5 font-weight-bold">
+              {{ appName }}にログイン
+            </h1>
+          </v-col>
 
+          <v-card
+            flat
+            width="80%"
+            max-width="320"
+            color="transparent"
+          >
+            <!-- ここからログインフォーム -->
+            <v-form
+              ref="form"
+            >
+              <v-text-field
+                label="メールアドレスを入力"
+                placeholder="your@email.com"
+                outlined
+              />
+              <v-text-field
+                label="パスワードを入力"
+                placeholder="8文字以上"
+                outlined
+              />
+              <v-btn
+                block
+                color="blue"
+                class="white--text"
+              >
+                ログインする
+              </v-btn>
+              <br>
+            </v-form>
+          </v-card>
+        </v-row>
+      </v-container>
+    <my-footer />
+  </v-app>
 </template>
-<script>
-export default {};
-</script>
-<style scoped>
 
-</style>
+<script>
+import myHeader from '~/layouts/loginheader.vue'
+import myFooter from '~/layouts/footer.vue'
+
+export default {
+  components: {
+    myHeader,
+    myFooter
+  },
+  data: () => ({
+    appName: "20時間タイマー"
+  })
+}
+</script>
