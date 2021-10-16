a4a0d6bf33cd4d5a75b4375b0bb697d5ecc943fc
Noriyuki <norifxsc@gmail.com>
2021-10-16 15:05:49 +0900

    success register firebase

--- pages/newuser.vue
+++ pages/newuser.vue
@@ -28,22 +28,26 @@
               <v-text-field
                 label="メールアドレスを入力"
                 placeholder="your@email.com"
+                v-model.trim="email"
                 outlined
               />
               <v-text-field
                 label="パスワードを入力"
                 placeholder="8文字以上"
+                v-model.trim="password"
                 outlined
               />
               <v-text-field
                 label="パスワードを再入力"
                 placeholder="8文字以上"
+                v-model.trim="passwordCheckVal"
                 outlined
               />
               <v-btn
                 block
                 color="blue"
                 class="white--text"
+                @click="register()"
               >
                 登録する
               </v-btn>
@@ -59,14 +63,35 @@
 <script>
 import myHeader from '~/layouts/loginheader.vue'
 import myFooter from '~/layouts/loginfooter.vue'
-
+import axios from 'axios'
 export default {
   components: {
     myHeader,
     myFooter
   },
   data: () => ({
-    appName: "20時間タイマー"
-  })
+    appName: "20時間タイマー",
+    email: '',
+    password: '',
+    passwordCheckVal: '', 
+  }),
+  methods: {
+    register() {
+      axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp', {
+        email: this.email,
+        password: this.password,
+        returnSecureToken: true,
+      },
+      {
+      params: {
+          key: process.env.apiKey,
+        },
+      }
+      )
+      .then(responce => {
+        console.log(responce);
+      })
+    }
+  }
 }
 </script>
