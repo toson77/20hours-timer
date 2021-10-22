5cec8cfc8fd2354dc7c2f49bc87a1b272c69fe87
Noriyuki <norifxsc@gmail.com>
2021-10-23 01:18:19 +0900

    login success

--- pages/login.vue
+++ pages/login.vue
@@ -1,59 +1,60 @@
 <template>
   <v-app>
     <my-header />
-      <v-container fluid>
-        <v-row
-          align="center"
-          justify="center"
+    <v-container fluid>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          class="my-8 text-center"
         >
-          <v-col
-            cols="12"
-            class="my-8 text-center"
-          >
-            <h1 class="text-h5 font-weight-bold">
-              {{ appName }}にログイン
-            </h1>
-          </v-col>
+          <h1 class="text-h5 font-weight-bold">
+            {{ appName }}にログイン
+          </h1>
+        </v-col>
 
-          <v-card
-            flat
-            width="80%"
-            max-width="320"
-            color="transparent"
-          >
-            <!-- ここからログインフォーム -->
-            <v-form
-              ref="form"
+        <v-card
+          flat
+          width="80%"
+          max-width="320"
+          color="transparent"
+        >
+          <!-- ここからログインフォーム -->
+          <v-form ref="form">
+            <v-text-field
+              label="メールアドレスを入力"
+              placeholder="your@email.com"
+              v-model.trim="email"
+              outlined
+            />
+            <v-text-field
+              label="パスワードを入力"
+              placeholder="8文字以上"
+              v-model.trim="password"
+              outlined
+            />
+            <v-btn
+              block
+              color="blue"
+              class="white--text"
+              @click="login()"
             >
-              <v-text-field
-                label="メールアドレスを入力"
-                placeholder="your@email.com"
-                outlined
-              />
-              <v-text-field
-                label="パスワードを入力"
-                placeholder="8文字以上"
-                outlined
-              />
-              <v-btn
-                block
-                color="blue"
-                class="white--text"
-              >
-                ログインする
-              </v-btn>
-              <br>
-            </v-form>
-          </v-card>
-        </v-row>
-      </v-container>
+              ログインする
+            </v-btn>
+            <br>
+          </v-form>
+        </v-card>
+      </v-row>
+    </v-container>
     <my-footer />
   </v-app>
 </template>
 
 <script>
-import myHeader from '~/layouts/loginheader.vue'
-import myFooter from '~/layouts/loginfooter.vue'
+import myHeader from "~/layouts/loginheader.vue";
+import myFooter from "~/layouts/loginfooter.vue";
 
 export default {
   components: {
@@ -61,7 +62,30 @@ export default {
     myFooter
   },
   data: () => ({
-    appName: "20時間タイマー"
-  })
-}
+    appName: "20時間タイマー",
+    email: "",
+    password: ""
+  }),
+  methods: {
+    login() {
+      axios
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword",
+          {
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true
+          },
+          {
+            params: {
+              key: process.env.apiKey
+            }
+          }
+        )
+        .then(responce => {
+          console.log(responce);
+        });
+    }
+  }
+};
 </script>
